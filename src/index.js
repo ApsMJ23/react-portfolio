3846690ca2face62a1b541b5d166491b578cc41c
apsmj23 <apurvalion@gmail.com>
2021-06-02 15:02:11 +0530

    FIxed the routing System

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import './bootstrap.min .css'
 import App from './App';
 
 ReactDOM.render(
-  <App />,
-
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
