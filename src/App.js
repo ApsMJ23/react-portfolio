92c3d979f0ca9dfedbe7420ae60efbe74aafafac
apsmj23 <apurvalion@gmail.com>
2021-06-02 15:31:55 +0530

    FIxed the routing System

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import ReactProjectScreen from './Screens/ReactProjectScreen'
-import HomeScreen from './Screens/HomeScreen'
-import ResumeScreen from './Screens/ResumeScreen'
-import ProShopScreen from './Screens/ProShopScreen'
-import CocktailsScreen from './Screens/CocktailsScreen'
+import { ReactProjectScreen } from './Screens/ReactProjectScreen'
+import { HomeScreen } from './Screens/HomeScreen'
+import { ResumeScreen } from './Screens/ResumeScreen'
+import { ProShopScreen } from './Screens/ProShopScreen'
+import { CocktailsScreen } from './Screens/CocktailsScreen'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
@@ -13,11 +13,11 @@ function App() {
     <Router>
       <Header />
       <Switch>
-        <Route path='/' exact component={<HomeScreen />} />
-        <Route path='/proshop' component={<ProShopScreen />} />
-        <Route path='/cocktails' component={<CocktailsScreen />} />
-        <Route path='/reactprojects' component={<ReactProjectScreen />} />
-        <Route path='/resume' component={<ResumeScreen />} />
+        <Route path='/' exact component={HomeScreen} />
+        <Route path='/proshop' component={ProShopScreen} />
+        <Route path='/cocktails' component={CocktailsScreen} />
+        <Route path='/reactprojects' component={ReactProjectScreen} />
+        <Route path='/resume' component={ResumeScreen} />
       </Switch>
       <Footer />
     </Router>
